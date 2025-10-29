37f5c7e562d067676d2815ef250609503870452e
sumaity42 <sumaity@syr.edu>
2025-10-29 19:12:41 -0400

    Create ML_Pred.py

---
+++ ML_Pred.py
@@ -0,0 +1,48 @@
+def ML_Model(data):
+	X = data.iloc[:, :-1]
+	y = data['label']
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+	scaler = StandardScaler()
+	scaled_X_train = scaler.fit_transform(X_train)
+	scaled_X_test = scaler.transform(X_test)
+
+	# Random Forest Classifier Model
+	RF = RandomForestClassifier(max_depth=50, max_features=80, min_samples_leaf=2,
+								n_estimators=400)
+
+	#RF.fit(X_train, y_train)
+	RF.fit(scaled_X_train, y_train)
+	
+	#RF.score(X_test, y_test)
+	RF.score(scaled_X_test, y_test)
+
+	feature_importances = RF.feature_importances_
+	feature_names = data.columns
+
+	sorted_feature_imp = sorted(list(zip(feature_names, feature_importances.flatten())), 
+								key=lambda x:x[1], reverse=True)
+
+	# Save sorted importance data
+	Name = 'Feature_Importance_RF_R121S_Y126F.pkl'
+	with open(Name, "wb") as fp:
+ 		pickle.dump(sorted_feature_imp, fp)
+		
+	# Plot feature Importance
+	n_feat = 10
+	List = []
+
+	for i in range(0, n_feat):
+    	List.append(sorted_feature_imp[i][0])
+
+	top_10_feat = data[List]
+
+	plt.rcParams['font.size'] = 15
+	plt.rcParams['axes.linewidth'] = 1.5
+
+	# Plot top {n_feat} feature importance
+	plot_feat_Imp(sorted_feature_imp, n_feat, 'RF_Top20_Feature_Imp_R121S_Y126F.png')
+
+	# Plot scatter matrix for top {n_feat} feature
+	plot_scatter_mat(data, top_10_feat, 'Scatter_Matrix_Top10_Feat_R121S_Y126F.png')
